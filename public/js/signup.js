46a839d1a676afa3543c9e33993f367673278b4e
Chris Waje <chriswaje324@gmail.com>
2024-01-21 10:08:28 -0800

    ADDED: frontend JS for signup form

---
+++ public/js/signup.js
@@ -0,0 +1,29 @@
+const { application } = require("express");
+
+const signupFormHandler = async function (event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-input-signup').value.trim();
+    const password = document.querySelector('#password-input-signup').value.trim();
+
+    if (password.length >= 8 && username) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+            headers: { 'Content-Type': application/json}
+        });
+
+        if (response.ok) {
+            document.location.replace('/')
+        } else {
+            alert('Sign-up failed')
+        }
+    } else {
+        alert('Please include both fields. Ensure that password is atleast 8 characters long');
+    }
+};
+
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
