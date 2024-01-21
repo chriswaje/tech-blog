dbc9c6f54d3d5aa67e943c99a03373bb7a254c38
Chris Waje <chriswaje324@gmail.com>
2024-01-21 10:16:20 -0800

    ADDED: frontend JS for login

---
+++ public/js/login.js
@@ -0,0 +1,23 @@
+const loginFormHandler = async function (event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-input-login').value.trim();
+    const password = document.querySelector('#password-input-login').value.trim();
+
+    const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: username,
+            password: password,
+        }),
+        headers: { 'Content-Type': 'application/json'}
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Login Failed')
+    }
+};
+
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,5 +1,3 @@
-const { application } = require("express");
-
 const signupFormHandler = async function (event) {
     event.preventDefault();
 
