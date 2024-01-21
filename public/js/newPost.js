d39d5c9df936d37f319851846e3295a4e6c16986
Chris Waje <chriswaje324@gmail.com>
2024-01-21 10:30:30 -0800

    ADDED: new post JS

---
+++ public/js/newPost.js
@@ -0,0 +1,23 @@
+const newPostHandler = async function (event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const body = document.querySelector('textarea[name="post-body]').value;
+
+    const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            body
+        }),
+        hedaers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard')
+    } else {
+        alert('Failed to create new post')
+    }
+};
+
+document.querySelector('#new-post-form').addEventListener('submit', newPostHandler);
