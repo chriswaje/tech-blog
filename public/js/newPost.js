a5440c12b1d889836536656d23fb1d41da67dbc3
Chris Waje <chriswaje324@gmail.com>
2024-01-21 10:37:38 -0800

    ADDED: comment feature JS

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -2,7 +2,7 @@ const newPostHandler = async function (event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value;
-    const body = document.querySelector('textarea[name="post-body]').value;
+    const body = document.querySelector('textarea[name="post-body"]').value;
 
     const response = await fetch('/api/posts', {
         method: 'POST',
