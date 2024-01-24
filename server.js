aa2c5d018016de04257a40da84671ca05e19eef7
Chris Waje <chriswaje324@gmail.com>
2024-01-23 17:56:09 -0800

    ADDED: session secret

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'Super secret secret',
+    secret: process.env.SESSION_SECRET,
     cookie: {
         maxAge: 1000*600,
         httpOnly: true,
